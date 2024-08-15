8501054944eb25ca09367947b4a88d3aa6c7ee25
Kinenystuen <kine_on@hotmail.com>
2024-08-15 13:19:27 +0200

    github recrets test

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents() {
+    setupNodeEvents(on, config) {
       // implement node event listeners here
+      config.env.email = process.env.email;
+      config.env.password = process.env.password;
     },
   },
 });
