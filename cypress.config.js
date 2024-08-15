6b8b21f29c9c7adfbeeb99bc27b146c24816af6e
Kinenystuen <kine_on@hotmail.com>
2024-08-15 13:40:03 +0200

    env secrets

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'cypress';
+import dotenv from 'dotenv';
+
+// Last inn miljøvariablene
+dotenv.config();
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      config.env.email = process.env.email;
-      config.env.password = process.env.password;
     },
+  },
+  env: {
+    email: process.env.email,
+    password: process.env.password,
   },
 });
